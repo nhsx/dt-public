23a34eba06bb788ea6e5b6b0e15b2b2f7548db46
Alex Smith <88447828+asmith-nhsx@users.noreply.github.com>
2021-12-24 12:09:02 +0000

    Python script to rebase includes in puml files

---
+++ .github/workflows/rebase-includes.py
@@ -0,0 +1,29 @@
+import re
+import os
+import glob
+from pathlib import Path
+
+def replaceInFile(filepath, pattern, replace):
+    with open(filepath, 'r') as file:
+        data = file.read()
+        if (re.search(pattern, data) is not None):
+            print("matched", filepath, pattern, replace)
+            data = re.sub(pattern, "\\1" + replace + "/\\2", data)
+            with open(filepath, 'w') as file:
+                file.write(data)
+        elif (os.path.exists(filepath + ".rep")):
+            os.remove(filepath + ".rep")
+
+def replaceInFiles(root, path, pattern, newbase):
+    rootPath = Path(root)
+    for f in glob.glob(path, recursive=True):
+        replace = newbase + str(Path(f).parent.relative_to(rootPath)).replace("\\", "/")
+        replaceInFile(f, pattern, replace)
+
+
+if __name__ == '__main__':
+
+    replaceInFiles(r"G:\My Drive\architecture",
+                   r"G:\My Drive\architecture\**\*.puml",
+                   "([!]include\s+?)([^<:]+?\n)",
+                   "https://raw.githubusercontent.com/nhsx/dt-public/main/")
